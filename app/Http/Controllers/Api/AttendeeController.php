9652b19ce698fdff05d89983dc141c6b8cee0995
Renate <97016086@st.deltion.nl>
2024-03-21 10:31:06 +0100

    made the attendee relations

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -4,59 +4,66 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Attendee;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(Event $event)
-    {
-        $attendees  = $event->attendees()->latest();
+  use CanLoadRelationships;
 
-        return AttendeeResource::collection(
-            $attendees->paginate()
-        );
-    }
+  private array $relations  = ['user'];
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request, Event $event)
-    {
-        $attendee = $event->attendees()->create([
-          'user_id' => 1
-        ]);
+  public function index(Event $event)
+  {
+    $attendees  = $this->loadRelationships(
+      $event->attendees()->latest()
+    );
 
-        return new AttendeeResource($attendee);
-    }
+    return AttendeeResource::collection(
+      $attendees->paginate()
+    );
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Event $event,  Attendee  $attendee)
-    {
-        return  new AttendeeResource($attendee);
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request, Event $event)
+  {
+    $attendee = $this->loadRelationships(
+      $event->attendees()->create([
+        'user_id' => 1
+      ])
+    );
+    return new AttendeeResource($attendee);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Event $event,  Attendee  $attendee)
+  {
+    return  new AttendeeResource(
+      $this->loadRelationships($attendee)
+    );
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $event, Attendee $attendee)
-    {
-        $attendee->delete();
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, string $id)
+  {
+    //
+  }
 
-        return  response(status:204);
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(string $event, Attendee $attendee)
+  {
+    $attendee->delete();
+
+    return  response(status: 204);
+  }
 }
