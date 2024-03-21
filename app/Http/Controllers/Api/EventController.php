f738a3495e9de6d6ded006fff15f713bbb667278
Renate <97016086@st.deltion.nl>
2024-03-21 09:54:28 +0100

    made the universal relation loading trait.
    this was an optional part of the tutorial.

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,98 +4,81 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $query =  Event::query();
-        $relations  = ['user', 'attendees', 'attendees.user'];
 
-        foreach ($relations as  $relation){
-            $query->when(
-              $this->shouldIncludeRelation($relation),
-              fn($q)	=>	$q->with($relation)
-            );
-        }
+	use	CanLoadRelationships;
 
-        return EventResource::collection(
-          $query->latest()->paginate()
-        );
-    }
+	private array	$relations	=	['user', 'attendees', 'attendees.user'];
+	/**
+	 * Display a listing of the resource.
+	 */
+	public function index()
+	{
 
-    protected function  shouldIncludeRelation(string  $relation): bool
-    {
-        $include  = request()->query('include');
+		$query = $this->loadRelationships(Event::query());
+		return EventResource::collection(
+			$query->latest()->paginate()
+		);
+	}
 
-        if  (!$include)  {
-          return  false;
-        }
 
-        $relations  = array_map('trim', explode(',',  $include));
-        
-        return  in_array($relation, $relations);
-    }
-    
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-      
-      $event = Event::create([
-        ...$request->validate([
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'start_time' => 'required|date',
-            'end_time' => 'required|date|after:start_time'
-        ]),
-        'user_id' => 1
-    ]);
+	/**
+	 * Store a newly created resource in storage.
+	 */
+	public function store(Request $request)
+	{
 
-    return new EventResource($event);
+		$event = Event::create([
+			...$request->validate([
+				'name' => 'required|string|max:255',
+				'description' => 'nullable|string',
+				'start_time' => 'required|date',
+				'end_time' => 'required|date|after:start_time'
+			]),
+			'user_id' => 1
+		]);
 
-    }
+		return new EventResource($this->loadRelationships($event));
+	}
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Event $event)
-    {
-        $event->load('user', 'attendees');
-        return new EventResource($event);
-    }
+	/**
+	 * Display the specified resource.
+	 */
+	public function show(Event $event)
+	{
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Event $event)
-    {
-          $event->update(
-            $request->validate([
-            'name' => 'sometimes|string|max:255',
-            'description' => 'nullable|string',
-            'start_time' => 'sometimes|date',
-            'end_time' => 'sometimes|date|after:start_time'
-        ])
-      );
+		return new EventResource($this->loadRelationships($event));
+	}
 
-      return  $event;
-        
-    }
+	/**
+	 * Update the specified resource in storage.
+	 */
+	public function update(Request $request, Event $event)
+	{
+		$event->update(
+			$request->validate([
+				'name' => 'sometimes|string|max:255',
+				'description' => 'nullable|string',
+				'start_time' => 'sometimes|date',
+				'end_time' => 'sometimes|date|after:start_time'
+			])
+		);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Event $event)
-    {
-        $event->delete();
+		return  new EventResource($this->loadRelationships($event));
+	}
 
-        return response(status: 204);
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 */
+	public function destroy(Event $event)
+	{
+		$event->delete();
+
+		return response(status: 204);
+	}
 }
