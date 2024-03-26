70ce76041fc8e7f6c9f9902551742bb71f9cb3e0
Renate <97016086@st.deltion.nl>
2024-03-26 11:15:03 +0100

    revoked some tokens
    made a change to how long the cookies will be stored and also revoked some tokens and logged the user out.

--- app/Http/Controllers/Api/Authcontroller.php
+++ app/Http/Controllers/Api/Authcontroller.php
@@ -9,35 +9,40 @@ use Illuminate\Validation\ValidationException;
 
 class Authcontroller extends Controller
 {
-	public  function  login(Request $request)
-	{
-		$request->validate([
-			'email' =>  'required|email',
-			'password'  =>  'required'
-		]);
+  public  function  login(Request $request)
+  {
+    $request->validate([
+      'email' =>  'required|email',
+      'password'  =>  'required'
+    ]);
 
-		$user = \App\Models\User::where('email',  $request->email)->first();
+    $user = \App\Models\User::where('email',  $request->email)->first();
 
-		if (!$user) {
-			throw ValidationException::withMessages([
-				'email' =>  ['The provided credentials are incorrect.']
-			]);
-		}
+    if (!$user) {
+      throw ValidationException::withMessages([
+        'email' =>  ['The provided credentials are incorrect.']
+      ]);
+    }
 
-		if (!Hash::check($request->password, $user->password)) {
-			throw ValidationException::withMessages([
-				'email'  => ['The provided credentials are incorrect.']
-			]);
-		}
+    if (!Hash::check($request->password, $user->password)) {
+      throw ValidationException::withMessages([
+        'email'  => ['The provided credentials are incorrect.']
+      ]);
+    }
 
-		$token	=	$user->createToken('api-token')->plainTextToken;
+    $token  =  $user->createToken('api-token')->plainTextToken;
 
-		return	response()->json([
-			'token'	=>	$token
-		]);
-	}
+    return  response()->json([
+      'token'  =>  $token
+    ]);
+  }
 
-	public  function  logout(Request  $request)
-	{
-	}
+  public  function  logout(Request  $request)
+  {
+    $request->user()->tokens()->delete();
+
+    return  response()->json([
+      'message' =>  'Logged out succesfully'
+    ]);
+  }
 }
